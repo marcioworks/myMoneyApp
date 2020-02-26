3a3e841789ed4ab50da0f586b45d16b9ced7c384
Marcio silva <marcioadsworks@gmail.com>
2020-02-26 15:03:41 -0300

    template dependecies

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,5 +1,8 @@
+import '../commons/template/dependencies'
 import React from 'react'
 
-export default props =>(
-    <div className="wrapper">App</div>
+export default props => (
+    <div className="wrapper">
+        <h1>App</h1>
+    </div>
 )

cb7de53f1b14a3239673fc0fbdf165f1cb785ba7
Marcio silva <marcioadsworks@gmail.com>
2020-02-29 11:34:53 -0300

    value boxes not on the same row

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,20 +1,29 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import ContentHeader from '../commons/template/contetHeader'
 import Content from '../commons/template/content'
+import ValueBox from '../commons/widget/valueBox'
+import Row from '../commons/layout/row'
 
 
-class Dashboard extends Component{
-    render(){
-        return(
+
+class Dashboard extends Component {
+    render() {
+        return (
             <div>
-                <ContentHeader title='Dashboard' small='version 1.0'/>
-                    <Content>
-                        Dashboard
-                    </Content>
-               
+                <ContentHeader title='Dashboard' small='version 1.0' />
+                <Content>
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank'
+                            value='R$ 10' text='Total de Créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card'
+                            value='R$ 10' text='Total de Débitos' />
+                        <ValueBox cols='12 4' color='blue' icon='money'
+                            value='R$ 0' text='Saldo' />
+                    </Row>
+                </Content>
+
             </div>
         )
     }
 }
 export default Dashboard
-   

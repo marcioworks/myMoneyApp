35fd8779b4f538b8e42cc5b5322fd2d25e191761
Marcio silva <marcioadsworks@gmail.com>
2020-02-29 13:26:07 -0300

    rootReducer component

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import App from './main/app'
+import reducers from './main/reducers'
+
+const store = createStore(reducers)
 
 ReactDom.render(
-    <App></App>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('app')
 )

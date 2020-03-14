f896f1ff6e3fa4c292335a6b98168cf38ae9859c
Marcio silva <marcioadsworks@gmail.com>
2020-03-13 23:35:16 -0300

    billingCycle action and reducer

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux'
 import DashBoardReducers from '../dashboard/dashboardReducers'
 import TabReducers from '../commons/tabs/tabReducers'
+import BillingCycleReducers from '../billingCycle/billingCycleReducers'
 
 const rootReducer = combineReducers({
     dashboard: DashBoardReducers,
-    tab: TabReducers
+    tab: TabReducers,
+    billingCycles: BillingCycleReducers
 })
 
 export default rootReducer
